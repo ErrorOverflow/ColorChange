c2a076820bd7253005f49d27ec40e318f2c38f5d
wmlbuaa <wmlbuaa@163.com>
2019-03-13 19:47:03 +0800

    change HSV

--- ColorChange.py
+++ ColorChange.py
@@ -3,35 +3,43 @@ import numpy as np
 import operator
 
 params = {
-    'red': [240, 15, 15],
-    'yellow': [255, 255, 0],
-    'green': [0, 255, 0],
-    'blue': [0, 0, 255],
-    'gray': [128, 128, 128],
-    'white': [255, 255, 255],
-    'black': [0, 0, 0]
+    'red': 0,
+    'orange': 11
 }
 
 
-def color_change(img, pixel, color):
-    r = np.average(img[70:90, 60:90][0])
-    g = np.average(img[70:90, 60:90][1])
-    b = np.average(img[70:90, 60:90][2])
-    for x in range(70, 90):
-        for y in range(60, 90):
-            if operator.eq(img[x, y], [255, 255, 255]):
-                img[x, y][0] = (params.get(color)[0] + img[x, y][0] - r)
-                img[x, y][1] = (params.get(color)[1] + img[x, y][1] - g)
-                img[x, y][2] = (params.get(color)[2] + img[x, y][2] - b)
-    cv.imshow("wee", img)
+def color_change(img, img_hsv, color):
+    # solve h
+    for x in range(img.shape[1]):
+        for y in range(img.shape[0]):
+            r = img[x, y][0]
+            g = img[x, y][1]
+            b = img[x, y][2]
+            max = np.max(img[x, y])
+            min = np.min(img[x, y])
+            if max == min:
+                h = 0
+            elif np.argmax(img[x, y]) == 0 and g >= b:
+                h = 60 * (g - b) / (max - min)
+            elif np.argmax(img[x, y]) == 0 and g < b:
+                h = 60 * (g - b) / (max - min) + 360
+            elif np.argmax(img[x, y]) == 1:
+                h = 60 * (b - r) / (max - min) + 120
+            else:
+                h = 60 * (r - g) / (max - min) + 240
+            if max == 0:
+                s = 0
+            else:
+                s = 1 - min / max
+            v = max
+            h = params.get(color)
+            img_hsv[x, y] = [h, s, v]
+    cv.imshow('res', img_hsv)
     cv.waitKey(0)
     return 0
 
 
 if __name__ == '__main__':
-    img = cv.imread('image/img000001.png')
-    gray_img = cv.imread('image/img000001.png', 0)
-    pixel = [[1 for __ in range(img.shape[0])] for _ in range(img.shape[1])]
-    cv.imshow("gray", gray_img)
-
-    color_change(img, pixel, 'red')
+    img = cv.imread('image/img.jpg')
+    img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    color_change(img, img_hsv, 'red')
